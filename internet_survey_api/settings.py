cccc7d6aefc2221c59ad21061a3fadf43275f75d
notebook_linux <misteriska@ya.ru>
2021-07-14 10:13:59 +0300

    Исправлен баг с путем к БД

--- internet_survey_api/settings.py
+++ internet_survey_api/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'internet_survey_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
